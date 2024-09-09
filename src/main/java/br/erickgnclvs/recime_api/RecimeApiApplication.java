42a088465b42bc012fb27efa103c289c9ba737f6
erick <erickgoncalves5@hotmail.com>
2024-09-09 17:26:26 -0300

    Initial commit

---
+++ src/main/java/br/erickgnclvs/recime_api/RecimeApiApplication.java
@@ -0,0 +1,13 @@
+package br.erickgnclvs.recime_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecimeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/erickgnclvs/recime_api/RecimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.erickgnclvs.recime_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecimeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
